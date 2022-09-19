3874de67fb325648bc87afea6233aa7b420969df
sghatuwa <s.ghatuwa@gmail.com>
2022-09-19 23:56:19 +0545

    added grails 3

---
+++ grails3/IMS/grails-app/services/com/test/MainService.groovy
@@ -0,0 +1,12 @@
+package com.test
+
+import grails.transaction.Transactional
+import org.apache.commons.math3.fraction.BigFraction
+
+@Transactional
+class MainService {
+
+    def serviceMethod() {
+        BigFraction
+    }
+}
---
+++ grails3/IMSDomainPlugin/grails-app/services/com/test/InfoDomainService.groovy
@@ -0,0 +1,11 @@
+package com.test
+
+import grails.transaction.Transactional
+
+@Transactional
+class InfoDomainService {
+
+    def serviceMethod() {
+
+    }
+}
