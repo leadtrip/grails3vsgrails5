99c38c5a5c67b918b7bb7f87d12f24329e0d7c48
sghatuwa <s.ghatuwa@gmail.com>
2022-09-28 22:09:02 +0545

    Updated missing files

--- grails517/IMS/grails-app/services/com/test/MainService.groovy
+++ grails517/IMS/grails-app/services/com/test/MainService.groovy
@@ -1,12 +1,14 @@
 package com.test
 
 import grails.gorm.transactions.Transactional
-//import org.apache.commons.math3.fraction.BigFraction
+import org.apache.commons.math3.fraction.BigFraction
+import com.test.InfoDomainService
 
 @Transactional
 class MainService {
 
     def serviceMethod() {
-//        BigFraction
+        BigFraction
+        InfoDomainService infoDomainService;
     }
 }
