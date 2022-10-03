3ddb31acc43e02acab70583d7ce7379ea929a388
leadtrip <woodm@objectcomputing.com>
2022-10-03 08:54:52 +0100

    Gradle multi project changes

--- grails517/IMS/grails-app/services/com/test/MainService.groovy
+++ grails517/IMS/grails-app/services/com/test/MainService.groovy
@@ -1,14 +1,13 @@
 package com.test
 
 import grails.gorm.transactions.Transactional
-import org.apache.commons.math3.fraction.BigFraction
-import com.test.InfoDomainService
 
 @Transactional
 class MainService {
 
+    InfoDomainService infoDomainService
+
     def serviceMethod() {
-        BigFraction
-        InfoDomainService infoDomainService;
+
     }
 }
